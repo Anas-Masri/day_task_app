203faed77715ebd7bc8b477c77245cee450334b4
anasmasri994 <anasmasri2037@gmail.com>
2026-01-14 12:20:41 +0300

    add auth feature init

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: const MyHomePage(title: 'Flutter Demo Home Page'));
   }
 }
 
